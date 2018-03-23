a0797b6e8ef8e4d991210d52ea31d071afd98eec
Jakub Kopański <jkopanski@craftware.biz>
2018-03-23 11:19:55 +0100

    consts, size and depth for terms

--- arith/src/Syntax.hs
+++ arith/src/Syntax.hs
@@ -1,5 +1,9 @@
 module Syntax where
 
+import Data.Semigroup ((<>))
+import Data.Set (Set)
+import qualified Data.Set as Set
+
 data TmBool = TmTrue | TmFalse
   deriving (Eq, Show)
 
@@ -8,6 +12,14 @@ data Lit
   | LZero
   deriving (Eq, Show)
 
+instance Ord Lit where
+  (LBool TmFalse) <= LZero = True
+  (LBool TmTrue)  <= LZero = False
+  (LBool TmFalse) <= (LBool TmTrue)   = True
+  (LBool TmTrue)  <= (LBool TmFalse) = False
+  LZero <= (LBool TmTrue)  = True
+  LZero <= (LBool TmFalse) = True
+
 data Term
   = TmLit Lit
   | TmCond Term Term Term
@@ -15,3 +27,24 @@ data Term
   | TmPred Term
   | TmTest Term
   deriving (Eq, Show)
+
+consts :: Term -> Set Lit
+consts (TmLit l) = Set.singleton l
+consts (TmCond t1 t2 t3) = consts t1 <> consts t2 <> consts t3
+consts (TmSucc t) = consts t
+consts (TmPred t) = consts t
+consts (TmTest t) = consts t
+
+size :: Term -> Int
+size (TmLit l) = 1
+size (TmCond t1 t2 t3) = 1 + size t1 + size t2 + size t3
+size (TmSucc t) = 1 + size t
+size (TmPred t) = 1 + size t
+size (TmTest t) = 1 + size t
+
+depth :: Term -> Int
+depth (TmLit l) = 1
+depth (TmCond t1 t2 t3) = 1 + maximum [depth t1, depth t2, depth t3]
+depth (TmSucc t) = 1 + depth t
+depth (TmPred t) = 1 + depth t
+depth (TmTest t) = 1 + depth t
